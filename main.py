29390db7ba2351f51428c78b0ea79109179c59fd
JoaoGVP001 <joaogv.pazinato@gmail.com>
2025-04-29 10:36:49 -0300

    Primeiro commit

--- main.py
+++ main.py
@@ -4,7 +4,57 @@ import uuid
 app = Flask(__name__)
 
 historico_calculos = []
+from flask import Flask, request
+import uuid
+import psycopg
 
+app = Flask(__name__)
+connection_db = psycopg.connect(
+    dbname="joaoeallam",
+    user="postgres",
+    password="3f@db",
+    host="164.90.152.205",
+    port="80"
+)
+
+@app.route("/pessoas", methods=["POST"])
+def incluir_pessoa():
+    dados_recebidos = request.get_json()
+    id = uuid.uuid4()
+    nome = dados_recebidos['nome']
+
+    cursor = connection_db.cursor()
+    cursor.execute('insert into pessoas (id, nome) values (%s, %s)', (id, nome))
+    connection_db.commit()
+
+    return {
+        'id': id
+    }
+
+@app.route("/pessoas", methods=["GET"])
+def get_pessoas():
+    cursor = connection_db.cursor()
+    cursor.execute('select id, nome from pessoas order by nome')
+
+    lista = []
+    for item in cursor:
+        lista.append({
+            'id': item[0],
+            'nome': item[1]
+        })
+    return lista
+
+@app.route("/pessoas/<id>", methods=["PUT"])
+def atualizar_pessoas(id):
+    dados_recebidos = request.get_json()
+    nome = dados_recebidos['nome']
+
+    cursor = connection_db.cursor()
+    cursor.execute('update pessoas set nome = %s where id = %s', (nome, id))
+    connection_db.commit()
+    return {
+        'id': id
+    }
 @app.route("/soma", methods=["POST"])
 def somar():
     dados_recebidos = request.get_json()
@@ -44,6 +94,36 @@ def editar(id):
             return calculo
 
     return {"erro": "Cálculo não encontrado"}, 404
+
+@app.route("/vendas", methods=["GET"])
+def get_vendas():
+    cursor = connection_db.cursor()
+    cursor.execute('''select * from vendas
+left join pessoas on vendas.id_pessoa = pessoas.id
+left join produtos on vendas.id_produto = produtos.id''')
+    
+    lista = []
+    for item in cursor:
+        lista.append({
+            'id': item[0],
+            'pessoa': {
+                'id': item[1],
+                'nome': item[4]
+            },
+            'produto': {
+                'id': item[2],
+                'nome': item[6],
+                'preco': item[7]
+            }
+        })
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
